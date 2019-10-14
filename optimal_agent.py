8f6d3ede7fa616856851f75fd5e885a1dfde262f
“lurkereer” <watson.i.wang@gmail.com>
2019-10-14 10:04:49 -0700

    Update optimal control agent

--- optimal_agent.py
+++ optimal_agent.py
@@ -10,8 +10,24 @@ class Optimalagent:
         self.fric_r = constants.fr
         self.length = constants.l
         self.state = constants.x0
+        self.state_set = [self.state]
         self.action = []
+        self.action_set = []
+        self.goal = constants.goal
+        self.T = constants.T
 
 
+    def sys_gen(self):
+        theta = self.state[4]
+        T = self.T
+        cfr1 = self.fric_t
+        cfr2 = self.fric_r
+        M = self.mass
+        l = self.length
+        I = self.I
+        A = np.array([1, 0, T, 0, 0, 0], [0, 1, 0, T, 0, 0], [0, 0, (1-cfr1/M), 0, 0, 0], [0, 0, 0, (1-cfr1/M), 0, 0], [0, 0, 0, 0, 1, T],[0, 0, 0, 0, 0, 1-cfr2/I])
+        B1 = np.array([0], [0], [np.cos(theta)*T/M], [np.cos(theta)*T/M], [0], [T*l/I])
+        B2 = np.array([0], [0], [np.cos(theta)*T/M], [np.cos(theta)*T/M], [0], [-T*l/I])
+        return A, B1, B2
 
     def
--- parameters.py
+++ parameters.py
@@ -3,13 +3,16 @@ import numpy as  np
 class Parameters1:
 
     # System dynamic parameters
-    m =
-    l =
-    I =
-    ft =
-    fr =
-    goal =
-    goal_set =
+    m = 0.2
+    a = 0.3
+    b = 2
+    l = 0.5 * b
+    I = 0.067
+    ft = 0.02
+    fr = 0.0067
+    T = 0.01
+    # goal position
+    goal = np.array([1, 0, 0, 0, -np.pi * 0.5, 0])
+    # goal_set  = np.array([1,0,0,0,-np.pi * 0.5,0])
     # initial state
-    x0 =
-
+    x0 = np.array([0, 0, 0, 0, 0, 0])
