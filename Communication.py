56fbc3e454a9be4a5f4ba7d2ca70154c733072c2
pallavishintre <pshintre@asu.edu>
2019-10-18 19:58:12 -0700

    class for zmq

---
+++ Communication.py
@@ -0,0 +1,24 @@
+import zmq
+
+
+class Communication:
+
+    def __init__(self):
+        context = zmq.Context()
+        self.socket_in = context.socket(zmq.SUB)
+        self.socket_out = context.socket(zmq.PUB)
+        self.socket_in.connect("tcp://localhost:5577")
+        self.socket_out.bind("tcp://*:5578")
+
+    def rec(self):
+        topic_filter = b""
+        self.socket_in.setsockopt(zmq.SUBSCRIBE, topic_filter)
+        msg = self.socket_in.recv_string()
+        return msg
+
+    def send(self, data):
+        #  Send reply back to client
+        self.socket_out.send_string(data)
+
+
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from Communication import Communication
+from datetime import datetime
+
+
+print('Start')
+communicator = Communication()
+while True:
+    message = communicator.rec()
+    print(message)
+    # current date and time
+    now = datetime.now()
+    timestamp = datetime.timestamp(now)
+    print("timestamp =", timestamp)
+    communicator.send("Hello")
