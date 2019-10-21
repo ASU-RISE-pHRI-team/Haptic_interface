569c6138bbf1451279d97cb0aaeae40442902c79
pallavishintre <pshintre@asu.edu>
2019-10-21 13:00:38 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	communication_class.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from Communication import Communication
 from datetime import datetime
 
-
-
 print('Start')
 communicator = Communication()
 while True:
     message = communicator.rec()
     print(message)
     # current date and time
-    now = datetime.now()
-    timestamp = datetime.timestamp(now)
-    print("timestamp =", timestamp)
+    now = datetime.now().isoformat(timespec='microseconds')
+    print("timestamp =", now)
+    # timestamp = datetime.timestamp(now)
+    # print("timestamp =", timestamp)
     communicator.send("Hello")
