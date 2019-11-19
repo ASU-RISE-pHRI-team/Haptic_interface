803792ba2a7d78b6c602bca68aad60dcc4949094
pallavishintre <pshintre@asu.edu>
2019-11-19 14:21:13 -0700

    fix the writing csv file code.

--- sysid.py
+++ sysid.py
@@ -105,44 +105,39 @@ class Communication:
 
 
 def main():
-    agent = Optimalagent(Parameters1)
-    kim = Communication()
-    kim.run()
-    kim.runner1()
-    kim.runner2()
-    time.sleep(3)
-    print(kim.state)
-    state_set = [kim.state]
-    while True:
-        t1 = time.time()
-        kim.translate()
-        agent.state = kim.state
-        kim.state_set.append(kim.state)
-        state_set.append(kim.state)
-        print(agent.state)
-        msg1 = {"force_x": -5.0, "force_y": 0, "force_z": 0}
-        msg2 = {"force_x": -5.0, "force_y": 0, "force_z": 0}
-        msg1_json = json.dumps(msg1)
-        msg2_json = json.dumps(msg2)  # pallavi
-        kim.my_sender(msg2_json)  # pallavi
-        kim.send(msg1_json)
-        t2 = time.time()
-        if t2 - t1 - Parameters1.T < 0:
-            time.sleep(Parameters1.T - t2 + t1)
+    try:
+        agent = Optimalagent(Parameters1)
+        kim = Communication()
+        kim.run()
+        kim.runner1()
+        kim.runner2()
+        time.sleep(3)
+        print(kim.state)
+        state_set = [kim.state]
+        action = -5.0
+
+        while True:
+            t1 = time.time()
+            kim.translate()
+            agent.state = kim.state
+
+            print(agent.state)
+            msg1 = {"force_x": action, "force_y": 0, "force_z": 0}
+            msg2 = {"force_x": action, "force_y": 0, "force_z": 0}
+            msg1_json = json.dumps(msg1)
+            msg2_json = json.dumps(msg2)  # pallavi
+            kim.my_sender(msg2_json)  # pallavi
+            kim.send(msg1_json)
+            t2 = time.time()
+            agent.data_append(kim.state, action)
+            if t2 - t1 - Parameters1.T < 0:
+                time.sleep(Parameters1.T - t2 + t1)
+    except KeyboardInterrupt:
+        print("Aha")
+        np.savetxt("foo.csv", agent.state_set, delimiter=",")
+
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Answer")
+    main()
 
-    # with open('my_data.csv', mode='w') as data_file:
-    #     data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    #
-    #     data_writer.writerow(agent.state[0])
-    #     data_writer.writerow(agent.state[1])
-    #     data_writer.writerow(agent.state[2])
-    #     data_writer.writerow(agent.state[3])
-    #     data_writer.writerow(agent.state[4])
-    #     data_writer.writerow(agent.state[5])
