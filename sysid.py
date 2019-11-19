12dad31ff705406eac5df36f4296ffd94c31a56f
pallavishintre <pshintre@asu.edu>
2019-11-18 19:18:53 -0700

    System identification code

--- sysid.py
+++ sysid.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 from optimal_agent import Optimalagent
 from parameters import Parameters1
+import csv
 
 
 class Communication:
@@ -13,12 +14,15 @@ class Communication:
         self.context1 = zmq.Context()
         self.context2 = zmq.Context()
         self.context3 = zmq.Context()
+        self.context4 = zmq.Context()
         self.socket_in1 = self.context1.socket(zmq.SUB)
         self.socket_in2 = self.context2.socket(zmq.SUB)
-        self.socket_out = self.context3.socket(zmq.PUB)
+        self.socket_out1 = self.context3.socket(zmq.PUB)
+        self.socket_out2 = self.context4.socket(zmq.PUB)  # pallavi
         self.socket_in1.connect("tcp://127.0.0.1:5557")
         self.socket_in2.connect("tcp://127.0.0.1:5527")
-        self.socket_out.bind("tcp://127.0.0.1:7001")
+        self.socket_out1.bind("tcp://127.0.0.1:7001")
+        self.socket_out2.bind("tcp://127.0.0.1:7003")  # pallavi
 
         self.rec_1 = threading.Thread(target=self.rec1)
         self.rec_2 = threading.Thread(target=self.rec2)
@@ -29,12 +33,13 @@ class Communication:
         self.forces = {"force_x": -5.0, "force_y": 0, "force_z": 0}
         self.loc = 0
         # self.pos = 0
-        # self.r = 0
+        # self.r = 0k
         # self.v = 0
         # self.w = 0
         self.state = Parameters1.x0
         self.msg = {"force_x": -10.0, "force_y": 0, "force_z": 0}
         self.observed_action = []
+        self.state_set = [self.state]
 
     def rec1(self):
 
@@ -44,9 +49,6 @@ class Communication:
             forces = self.socket_in1.recv_string()
             self.forces = json.loads(forces)
 
-            # self.forces =
-            # print(self.forces)
-
     def rec2(self):
 
         topic_filter = b""
@@ -55,10 +57,13 @@ class Communication:
             self.loc = self.socket_in2.recv_string()
             # print(self.loc)
 
+    def my_sender(self, msg):  # pallavi
+        self.socket_out2.send_string(msg)
+
     def send(self, msg):
         # while True:
         #     #message = json.dumps(msg)
-        self.socket_out.send_string(msg)
+        self.socket_out1.send_string(msg)
         # print(msg)
 
     def runner1(self):
@@ -87,16 +92,15 @@ class Communication:
         # self.r = np.array(jr)
         # self.w = np.array(jw)
         # self.v = np.array(jv)
-        self.state = np.array([jp[0], jp[2], jv[0], jv[2], jr[1] / 180 * np.pi, jw[1]])
-        self.observed_action = self.forces["force_x"]
+        if jr[1] > 180:
+            angle = jr[1] - 360
+        else:
+            angle = jr[1]
+        self.state = np.array([jp[0], jp[2], jv[0], jv[2], -angle / 180 * np.pi, -jw[1]])
+        self.observed_action = np.array([self.forces["force_x"], -self.forces["force_z"]])
 
         # return self.pos, self.r, self.v, self.w
-
-    def msg_gen(self, action):
-
-        msg = {"force_x": action, "force_y": 0, "force_z": 0}
-
-        return msg
+    #
 
 
 def main():
@@ -109,23 +113,33 @@ def main():
     print(kim.state)
     while True:
         t1 = time.time()
-        # msg_dic = kim.forces
-        # msg_json = json.dumps(msg_dic)
         kim.translate()
         agent.state = kim.state
-        agent.other_action = kim.observed_action
-        A, B1, B2 = agent.sysgen()
-        U = agent.optimal_action(A, B1, B2)
-        msg = kim.msg_gen(U[0])
-        msg_json = json.dumps(msg)
-
-        print(msg_json)
-        # print(msg_json)
+        kim.state_set.append(kim.state)
+        print(agent.state)
+        msg1 = {"force_x": -2.0, "force_y": 0, "force_z": 0}
+        msg2 = {"force_x": -2.0, "force_y": 0, "force_z": 0}
+        msg1_json = json.dumps(msg1)
+        msg2_json = json.dumps(msg2)  # pallavi
+        kim.my_sender(msg2_json)  # pallavi
+        kim.send(msg1_json)
         t2 = time.time()
-        if t2 - t1 - 0.05 < 0:
-            time.sleep(0.05 - t2 + t1)
+        if t2 - t1 - Parameters1.T < 0:
+            time.sleep(Parameters1.T - t2 + t1)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Answer")
 
+    # with open('my_data.csv', mode='w') as data_file:
+    #     data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    #
+    #     data_writer.writerow(agent.state[0])
+    #     data_writer.writerow(agent.state[1])
+    #     data_writer.writerow(agent.state[2])
+    #     data_writer.writerow(agent.state[3])
+    #     data_writer.writerow(agent.state[4])
+    #     data_writer.writerow(agent.state[5])
