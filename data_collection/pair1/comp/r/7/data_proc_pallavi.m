1fcef69e3c41d515f59a61003710bbc78120cc3d
pallavishintre <pshintre@asu.edu>
2020-03-01 15:18:16 -0700

    first commit

---
+++ data_collection/pair1/comp/r/7/data_proc_pallavi.m
@@ -0,0 +1,58 @@
+action_1 = loadaction("action_h1.csv");
+action_2 = loadaction("action_h2.csv");
+[L1, L2] = size(action_1);
+[M1, M2] = size(action_2);
+
+for i=1:L1
+    if action_1(i)< 0.73 && action_1(i)> -0.73
+        action_1(i) = 0;
+    end
+    if action_1(i)< 2.5 && action_1(i)> 0.73
+        action_1(i) = 0.7;
+    end
+    if action_1(i)< 4.5 && action_1(i)> 2.5
+        action_1(i) = 1.4;
+    end
+    if action_1(i)> 4.5
+        action_1(i) = 2.1;
+    end
+    
+    if action_1(i)< -0.73 && action_1(i)> -2.5
+        action_1(i) = -0.7;
+    end
+    if action_1(i)< -2.5 && action_1(i)> -4.5
+        action_1(i) = -1.4;
+    end
+    if action_1(i)< -4.5
+        action_1(i) = -2.1;
+    end
+        
+end
+
+for i=1:M1
+    if action_2(i)< 0.73 && action_2(i)> -0.73
+        action_2(i) = 0;
+    end
+    if action_2(i)< 2.5 && action_2(i)> 0.73
+        action_2(i) = -0.7;
+    end
+    if action_2(i)< 4.5 && action_2(i)> 2.5
+        action_2(i) = -1.4;
+    end
+    if action_2(i)> 4.5
+        action_2(i) = -2.1;
+    end
+    
+    if action_2(i)< -0.73 && action_2(i)> -2.5
+        action_2(i) = 0.7;
+    end
+    if action_2(i)< -2.5 && action_2(i)> -4.5
+        action_2(i) = 1.4;
+    end
+    if action_2(i)< -4.5
+        action_2(i) = 2.1;
+    end
+        
+end
+csvwrite('new_action_h1.csv', action_1)
+csvwrite('new_action_h2.csv', action_2)
---
+++ data_collection/pair2/incomp/tr/reaching_time.m
@@ -0,0 +1,82 @@
+a1(:,1) = getdata("1\action_h1.csv");
+a1(:,2) = getdata("1\action_h2.csv");
+a2(:,1) = getdata("2\action_h1.csv");
+a2(:,2) = getdata("2\action_h2.csv");
+a3(:,1) = getdata("3\action_h1.csv");
+a3(:,2) = getdata("3\action_h2.csv");
+a4(:,1) = getdata("4\action_h1.csv");
+a4(:,2) = getdata("4\action_h2.csv");
+a5(:,1) = getdata("5\action_h1.csv");
+a5(:,2) = getdata("5\action_h2.csv");
+a6(:,1) = getdata("6\action_h1.csv");
+a6(:,2) = getdata("6\action_h2.csv");
+a7(:,1) = getdata("7\action_h1.csv");
+a7(:,2) = getdata("7\action_h2.csv");
+a8(:,1) = getdata("8\action_h1.csv");
+a8(:,2) = getdata("8\action_h2.csv");
+a9(:,1) = getdata("9\action_h1.csv");
+a9(:,2) = getdata("9\action_h2.csv");
+a10(:,1) = getdata("10\action_h1.csv");
+a10(:,2) = getdata("10\action_h2.csv");
+[L1, L2] = size(a1);
+temp = [];
+for i = L1:-1:1
+    if a1(i,1) ~= 0 || a1(i,2) ~= 0
+        temp(1,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a2(i,1) ~= 0 || a2(i,2) ~= 0
+        temp(2,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a3(i,1) ~= 0 || a3(i,2) ~= 0
+        temp(3,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a4(i,1) ~= 0 || a4(i,2) ~= 0
+        temp(4,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a5(i,1) ~= 0 || a5(i,2) ~= 0
+        temp(5,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a6(i,1) ~= 0 || a6(i,2) ~= 0
+        temp(6,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a7(i,1) ~= 0 || a7(i,2) ~= 0
+        temp(7,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a8(i,1) ~= 0 || a8(i,2) ~= 0
+        temp(8,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a9(i,1) ~= 0 || a9(i,2) ~= 0
+        temp(9,1) = i;
+        break;
+    end
+end
+for i = L1:-1:1
+    if a10(i,1) ~= 0 || a10(i,2) ~= 0
+        temp(10,1) = i;
+        break;
+    end
+end
